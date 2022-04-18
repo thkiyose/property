7cc11dcda840389d26c5681a80e67efe81e8b80d
haruka tezuka <th_kiyose@outlook.com>
2022-04-18 12:25:13 +0900

    セキュリティ上問題のある正規表現を修正

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,6 +1,6 @@
 class Prop < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
-  validates :rent, format: { with: /^[0-9]+$/ }
-  validates :years_old, format: { with: /^[0-9]+$/ }, length: { maximum: 3}
+  validates :rent, format: { with: /\A[0-9]\z/ }
+  validates :years_old, format: { with: /\A[0-9]\z/ }, length: { maximum: 3}
   validates :comment, length: { maximum: 300 }
 end
