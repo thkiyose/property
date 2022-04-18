d35c8ba48d0d9591e881440554f6503bbef8c3c7
haruka tezuka <th_kiyose@outlook.com>
2022-04-18 14:01:25 +0900

    賃料と築年数のバリデーションを変更

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -1,7 +1,7 @@
 class Prop < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50}
-  validates :rent, format: { with: /[0-9]/ }, allow_blank: true
+  validates :rent, numericality: { only_integer: true }, allow_blank: true
   validates :address, presence: true, length: { minimum: 5, maximum: 255}
-  validates :years_old, format: { with: /[0-9]/ }, length: { maximum: 3}, allow_blank: true
+  validates :years_old, numericality: { only_integer: true }, length: { maximum: 3}, allow_blank: true
   validates :comment, length: { maximum: 300 }
 end
