e622062fc3c9c960e271015795bf24ed78eab453
haruka tezuka <th_kiyose@outlook.com>
2022-04-18 12:22:58 +0900

    createアクションとストロングパラメータを定義

--- app/controllers/props_controller.rb
+++ app/controllers/props_controller.rb
@@ -6,4 +6,19 @@ class PropsController < ApplicationController
   def new
     @prop = Prop.new
   end
+
+  def create
+    @prop = Prop.new(prop_params)
+    if @prop.save
+      redirect_to props_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def prop_params
+    params.require(:prop).permit(:name,:rent,:years_old,:comment)
+  end
 end
